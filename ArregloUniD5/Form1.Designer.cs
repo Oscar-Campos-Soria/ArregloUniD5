092bf559ef6a056b8fc7ffb25c4fc82ff275f0c5
FALKON <campossoriao1@gmail.com>
2024-03-12 06:46:02 +0100

    Agregar archivos de proyecto.

---
+++ ArregloUniD5/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace ArregloUniD5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncrear = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.LBx = new System.Windows.Forms.ListBox();
+            this.txtLong = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btncrear
+            // 
+            this.btncrear.Location = new System.Drawing.Point(12, 56);
+            this.btncrear.Name = "btncrear";
+            this.btncrear.Size = new System.Drawing.Size(112, 71);
+            this.btncrear.TabIndex = 0;
+            this.btncrear.Text = "Crear Arreglo";
+            this.btncrear.UseVisualStyleBackColor = true;
+            this.btncrear.Click += new System.EventHandler(this.btncrear_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(130, 56);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(137, 71);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Continuar";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // LBx
+            // 
+            this.LBx.FormattingEnabled = true;
+            this.LBx.ItemHeight = 15;
+            this.LBx.Location = new System.Drawing.Point(342, 9);
+            this.LBx.Name = "LBx";
+            this.LBx.Size = new System.Drawing.Size(120, 94);
+            this.LBx.TabIndex = 3;
+            // 
+            // txtLong
+            // 
+            this.txtLong.Location = new System.Drawing.Point(12, 27);
+            this.txtLong.Name = "txtLong";
+            this.txtLong.Size = new System.Drawing.Size(311, 23);
+            this.txtLong.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Aqui ingresaras los numeros";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LBx);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtLong);
+            this.Controls.Add(this.btncrear);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncrear;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ListBox LBx;
+        private System.Windows.Forms.TextBox txtLong;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ArregloUniD5/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace ArregloUniD5
+{
+    public partial class Form1 : Form
+
+    {
+        private int[] arregloUnidimensional;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncrear_Click(object sender, EventArgs e)
+        {
+            int longitud;
+
+            if (int.TryParse(txtLong.Text, out longitud) && longitud > 0)
+            {
+                arregloUnidimensional = new int[longitud];
+                MessageBox.Show("Arreglo unidimensional creado con éxito. Por favor, ingrese los elementos.");
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese una longitud válida para el arreglo.");
+            }
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+
+            if (arregloUnidimensional != null)
+            {
+                // Ingresar elementos en el arreglo
+                for (int i = 0; i < arregloUnidimensional.Length; i++)
+                {
+                    using (var inputDialog = new Form())
+                    {
+                        var label = new Label() { Left = 20, Top = 20, Text = $"Ingrese el elemento {i + 1}:" };
+                        var textBox = new TextBox() { Left = 20, Top = 50 };
+                        var button = new Button() { Text = "Aceptar", Left = 20, Top = 80 };
+
+                        button.Click += (sender, e) =>
+                        {
+                            if (int.TryParse(textBox.Text, out int elemento))
+                            {
+                                arregloUnidimensional[i] = elemento;
+                                inputDialog.DialogResult = DialogResult.OK;
+                                inputDialog.Close();
+                            }
+                            else
+                            {
+                                MessageBox.Show("Por favor, ingrese un valor entero válido.");
+                            }
+                        };
+
+                        inputDialog.Controls.Add(label);
+                        inputDialog.Controls.Add(textBox);
+                        inputDialog.Controls.Add(button);
+
+                        inputDialog.AcceptButton = button;
+
+                        if (inputDialog.ShowDialog() != DialogResult.OK)
+                        {
+                            MessageBox.Show("Debe ingresar todos los elementos del arreglo.");
+                            return;
+                        }
+                    }
+                    // Calcular la suma, el promedio, el valor máximo y el valor mínimo
+                    int suma = arregloUnidimensional.Sum();
+                    double promedio = arregloUnidimensional.Average();
+                    int maximo = arregloUnidimensional.Max();
+                    int minimo = arregloUnidimensional.Min();
+
+                    {
+                        // Mostrar resultados
+                        MessageBox.Show($"Suma de los elementos: {suma}\nPromedio de los elementos: {promedio}\nValor máximo: {maximo}\nValor mínimo: {minimo}");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, cree primero el arreglo unidimensional.");
+            }
+
+
+                }
+            }
+
+            
+        }
+    
+
